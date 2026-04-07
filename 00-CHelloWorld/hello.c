cdd461f8d3be3f2ea048eee53f390464d181f2ac
unknown <urielcw54@gmail.com>
2026-04-07 11:34:20 -0300

    makefile

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    /*FILE *output;
+    output = fopen("filename.txt", "w");
+    fprintf(output, "Hello, World!\n");
+    fclose(output);*/
     printf("Hello, World!\n");
     return 0;
 }
